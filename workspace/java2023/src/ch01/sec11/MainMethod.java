10a3acaa3c545b6543aae6f6479c2ef9df749529
Naellu <119831581+Naellu@users.noreply.github.com>
2023-02-14 11:23:22 +0900

    test commit 2

---
+++ workspace/java2023/src/ch01/sec11/MainMethod.java
@@ -0,0 +1,7 @@
+package ch01.sec11;
+
+public class MainMethod {
+	public static void main(String[] args) {
+		System.out.println("mainMethod 실행");
+	}
+}
---
+++ workspace/java2023/src/ch01/sec11/MainShortCut.java
@@ -0,0 +1,18 @@
+package ch01.sec11;
+
+public class MainShortCut {
+//	main 입력후 Ctrl + space
+	public static void main(String[] args) {
+		// sysout
+		System.out.println("HI JAVA");
+		System.out.println("출력 되는 내용");
+		System.out.println("출력!");
+		
+//		주석 + /
+//		실행 Ctrl + F11
+//		창크기 Ctrl + m
+//		한 줄 지우기 Ctrl + d
+//		줄 옮기기 Alt + 방향키 위아래
+//		힌트얻기 Ctrl + space
+	}
+}
